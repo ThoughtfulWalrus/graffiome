e4226723b874f98ba71fe0f42c1253cd25e08c9e
i-made-that <rogerthomasson@gmail.com>
2015-03-25 19:58:34 -0700

    Manifest.json setup Create highlighter.js file

---
+++ src/content-scripts/highlighter.js
@@ -0,0 +1,67 @@
+var tabUrl = window.location.href.toString();
+var selection = window.getSelection();
+var ranges;
+
+var Color = 'Yellow';
+var Highlighter = rangy.createHighlighter();
+
+var Red = rangy.createClassApplier('Red');
+var Blue = rangy.createClassApplier('Blue');
+var Green = rangy.createClassApplier('Green');
+var Pink = rangy.createClassApplier('Pink');
+var Aqua = rangy.createClassApplier('Aqua');
+var Yellow = rangy.createClassApplier('Yellow');
+
+Highlighter.addClassApplier(Red);
+Highlighter.addClassApplier(Blue);
+Highlighter.addClassApplier(Green);
+Highlighter.addClassApplier(Pink);
+Highlighter.addClassApplier(Aqua);
+Highlighter.addClassApplier(Yellow);
+
+var reloadHighlights = function(){
+  chrome.storage.local.get( function (storage) {
+    if (storage[tabUrl]){
+      ranges = storage[tabUrl];
+      Highlighter.deserialize(ranges);
+    }
+  });
+};
+
+var saveHighlights = function(ranges){
+
+  chrome.
+
+  chrome.storage.local.get( function (storage) {
+    storage[tabUrl] = ranges;
+    chrome.storage.local.set(storage, function(){
+      console.log('saved higlights');
+    });
+  });
+};
+
+// Message Listener
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+   if (request.color) {
+    Color = request.color;
+   }
+   if (request.remove) {
+    console.log('remove')
+    Highlighter.removeAllHighlights();
+    ranges = Highlighter.serialize();
+    saveHighlights(ranges);
+   }
+  }
+);
+
+$(function() {
+  reloadHighlights();
+  $('body').mouseup( function() {
+    if ( selection.type === "Range" ){
+      Highlighter.highlightSelection(Color);
+      ranges = Highlighter.serialize();
+      saveHighlights(ranges);
+    }
+  });
+});
